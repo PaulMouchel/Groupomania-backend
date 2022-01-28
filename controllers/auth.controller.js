e5d849ad9ac97455046d41be22ddc9996db8d76e
PaulMouchel <paulmouchel@live.fr>
2022-01-28 22:54:27 +0400

    auth controller almost done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -21,27 +21,34 @@ exports.signup = (req, res, next) => {
       .catch(error => res.status(500).json({ error }));
   };
 
-  exports.login = (req, res, next) => {
-    // User.findOne({ email: req.body.email })
-    //   .then(user => {
-    //     if (!user) {
-    //       return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-    //     }
-    //     bcrypt.compare(req.body.password, user.password)
-    //       .then(valid => {
-    //         if (!valid) {
-    //           return res.status(401).json({ error: 'Mot de passe incorrect !' });
-    //         }
-    //         res.status(200).json({
-    //           userId: user._id,
-    //           token: jwt.sign(
-    //             { userId: user._id },
-    //             'RANDOM_TOKEN_SECRET',
-    //             { expiresIn: '24h' }
-    //           )
-    //         });
-    //       })
-    //       .catch(error => res.status(500).json({ error }));
-    //   })
-    //   .catch(error => res.status(500).json({ error }));
+  exports.login = async (req, res, next) => {
+    
+    try {
+      const { email, password } = req.body
+      const user = await prisma.user.findUnique({
+          where: {
+            email: email
+          }
+      })
+      if (!user) {
+        return res.status(401).json({ error: "Can't find user with this email" });
+      } 
+      bcrypt.compare(password, user.password)
+      .then(valid => {
+        if (!valid) {
+          return res.status(401).json({ error: 'Incorrect password' });
+        } 
+        res.status(200).json({
+          userId: user._id,
+          token: jwt.sign(
+            { userId: user._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' }
+            )
+            })
+          })
+    } catch (error) {
+        next(error)
+    }
+
   };
